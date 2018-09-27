894016cd76f86bc08c52d5b0ec3b1ffd6dc31894
Adam Munoz <adam@exolever.com>
2018-09-27 17:23:16 +0900

    2

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-requirejs([], () => {
-  console.debug('Starting app...');
-});
+requirejs.config({
+	paths: {
+          'react': 'https://unpkg.com/react@15.3.2/dist/react',
+          'react-dom': 'https://unpkg.com/react-dom@15.3.2/dist/react-dom'}});
+
+requirejs(['react', 'react-dom', 'view/counter'], 
+          (React, ReactDOM, Counter) => {
+  console.debug('starting app ...'); 
+  ReactDOM.render(React.createElement(Counter),
+                  document.getElementById('app'));});
---
+++ view/counter.js
@@ -0,0 +1,3 @@
+define(['react'], React =>
+  function Counter() {
+    return  React.createElement('p', {}, 'counter');});
