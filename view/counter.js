9d19267396c3d9a9bb45cedf9d3ef8df5f39c0b1
Adam Munoz <adam@exolever.com>
2018-10-02 12:05:26 +0900

    5
    click handler

--- view/counter.js
+++ view/counter.js
@@ -1,3 +1,10 @@
 define(['react'], React =>
   function Counter({n, text}) {
-    return  React.createElement('p', {}, `counter is ${n} - ${text}`);});
+
+    function onClickButton(e) {
+      console.debug(`click ${e.target.innerText}`);
+    }
+    return  React.createElement('div', {}, [
+              React.createElement('p',{key: 1}, `counter is ${n} - ${text}`),
+              React.createElement('button',
+                {key: 2, onClick: onClickButton}, 'increment')])});
